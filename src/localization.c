326bcf3d90224a59534bafd506d906dc63042520
Gabriel Tavares Barbosa <gabrieltavares.tic@gmail.com>
2025-01-22 02:48:58 -0300

    BIG BANG 💥

--- src/localization.c
+++ src/localization.c
@@ -9,7 +9,8 @@
 
 #include "../include/localization/localization.h"
 
-// Default language
+// Describes the current language set in the program.
+// Default is English.
 const Localization *currentLocalization = &localizations[LangTypeEn];
 
 int askLanguageChoice(void) {
@@ -21,12 +22,12 @@ int askLanguageChoice(void) {
     printf("Press Enter to select default language\n");
     printf("Choice: ");
 
-    // Lê a escolha do usuário e verifica se a entrada é válida
+    // Read the user's choice and check if the input is valid
     char inputBuffer[10];
     if (fgets(inputBuffer, sizeof(inputBuffer), stdin) != NULL) {
-        // Se o usuário digitou algo, tentamos converter para inteiro
+        // If the user typed something, try to convert to number
         if (sscanf(inputBuffer, "%d", &choice) != 1) {
-            // Se não for possível converter para número, define 0 (English)
+            // If it's not possible to convert to number, set to 0 (English)
             choice = 0;
         }
     }
@@ -35,11 +36,14 @@ int askLanguageChoice(void) {
 }
 
 void setLanguage(LanguageType type) {
-    if (type >= 0 && type < TotalNumberOfLanguages) {
-        currentLocalization = &localizations[type];
-        printf("%s\n\n", currentLocalization->languageSetMessage);
-    } else {
-        printf("Invalid language. Defaulting to English.\n\n");
-        currentLocalization = &localizations[LangTypePt];
+    if (type >= TotalNumberOfLanguages) {
+        printf("Invalid language choice. Defaulting to English.\n\n");
+        return;
     }
+
+    if (type > 0) {
+        currentLocalization = &localizations[type];
+    }
+
+    printf("%s\n\n", currentLocalization->languageSetMessage);
 }
